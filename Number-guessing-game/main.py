042373b39cd2b13d98e8e14d0a1de8625fc538f0
Amit Kumar <mysticbros227@gmail.com>
2026-01-14 10:12:58 +0530

    Add number guessing game in Python
    Implement a CLI-based number guessing game with attempts.

---
+++ Number-guessing-game/main.py
@@ -0,0 +1,40 @@
+"""
+Project: Number Guessing Game
+Author: Amit kumar
+Date: 2026-01-14
+Description: A CLI based game using Python loops and conditionals.
+"""
+
+import random
+secret_number=random.randint(1,100)
+attempts=10
+print("Welcome to the Number Guessing Game!")
+print("I have selected a number between 1 and 100")
+print("You will be given total 10 attempts to guess the number.")
+b="LET START"
+
+print(b.center(50))
+
+while attempts>=0:
+   
+    
+   
+    a=int(input("enter your guess:"))
+    
+    if a<secret_number:
+        print("You Guessed a incorrect number")
+        print("Your number is low from guessed number")
+        print("You have",attempts,"attempts left")
+    elif a>secret_number:    
+        print("You Guessed a incorrect number")
+        print("Your number is high from guessed number")
+        print("You have", attempts,"attempts left")
+    else:
+        print("Congratulations! You guessed the correct number:",secret_number)
+        break
+    if attempts==0:
+        print("Sorry, you've used all your attempts. The correct number was:",secret_number)
+        break
+    attempts=attempts-1
+
+
